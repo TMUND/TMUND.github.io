d9dc237f4def81b9c9493886786f19c76d6e40d7
toni <mundert@gmail.com>
2017-03-26 14:37:28 -0700

    reorganizing structure

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
-import './App.scss';
 import React, { Component } from 'react';
 import { browserHistory } from 'react-router';
 import Scroll from 'react-scroll';
 
+import BannerImage from './components/BannerImage';
 
-const Link = Scroll.Link;
-const Element = Scroll.Element;
+import './styles/BannerImage.scss';
+import './styles/App.scss';
+
+
+// const Link = Scroll.Link;
+// const Element = Scroll.Element;
 const Events = Scroll.Events;
 const scroll = Scroll.animateScroll;
 const scrollSpy = Scroll.scrollSpy;
@@ -20,6 +24,10 @@ class App extends Component {
     browserHistory.push(route);
   }
 
+  scrollToTop() {
+    scroll.scrollToTop();
+  }
+
   componentDidMount() {
     Events.scrollEvent.register('begin', function() {
       console.log("begin", arguments);
@@ -32,10 +40,6 @@ class App extends Component {
     scrollSpy.update();
   }
 
-  scrollToTop() {
-    scroll.scrollToTop();
-  }
-
   componentWillUnmount() {
     Events.scrollEvent.remove('begin');
     Events.scrollEvent.remove('end');
@@ -43,47 +47,12 @@ class App extends Component {
 
   render() {
     return (
-      <div className="app">
-        <div className='app-header'>
-
-          <div className='header-menu'>
-            <div className='menu-item home' onClick={ () => this.handleClick('/') }>
-              <li><Link activeClass="active" className="test1" to="test1" spy={true} smooth={true} duration={500} >Home</Link></li>
-            </div>
-
-            <div className='menu-item story' onClick={ () => this.handleClick('/venue') }>
-              <div className='header-text'>Venue</div>
-            </div>
-
-            <div className='menu-item names' onClick={ () => this.handleClick('/accomodation') }>
-              <div className='header-text'>Accomodation</div>
-            </div>
-
-            <div className='menu-item names' onClick={ () => this.handleClick('/day-of') }>
-              <div className='header-text'>Day-of Details</div>
-            </div>
-
-            <div className='menu-item registry' onClick={ () => this.handleClick('/registry') }>
-              <div className='header-text'>Registry</div>
-            </div>
-          </div>
-
-          <Element name="test1" className="element" >
-            Poo
-          </Element>
-          <Element name="test1" className="element" >
-            On
-          </Element>
-          <Element name="test1" className="element" >
-            You
-          </Element>
-          <Element name="test1" className="element" >
-            Home
-          </Element>
-          <Element name="test1" className="element" >
-            Home
-          </Element>
-        </div>
+      <div className='app'>
+        <BannerImage
+          className='home'
+          text='Time formula not correct'
+          counter={ true }
+        />
       </div>
     )
   }
---
+++ src/components/BannerImage.jsx
@@ -0,0 +1,71 @@
+import React, { Component } from 'react';
+
+
+class BannerImage extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      days: '00',
+      hours: '00',
+      minutes: '00',
+      seconds: '00'
+    }
+  }
+
+  componentDidMount() {
+    this.countdown();
+  }
+
+  countdown() {
+    return setInterval(() => {
+      let futureTime = new Date('Dec 20, 2017 16:00:00').getTime();
+      let currentTime = new Date().getTime();
+      let timeBetween = futureTime - currentTime;
+
+      let days = Math.floor(timeBetween / (1000 * 60 * 60 * 24));
+      let hours = Math.floor((timeBetween % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      let minutes = Math.floor((timeBetween % (1000 * 60 * 60)) / (1000 * 60));
+      let seconds = Math.floor((timeBetween % (1000 * 60)) / 1000);
+
+      if (timeBetween < 0 && this.refs.myRef) {
+        clearInterval();
+
+        this.setState({
+          days: '00',
+          hours: '00',
+          minutes: '00',
+          seconds: '00'
+        });
+      } else if (this.refs.myRef) {
+        this.setState({
+          days: days,
+          hours: hours,
+          minutes: minutes,
+          seconds: seconds
+        });
+      }
+    }, 1);
+  }
+
+  render() {
+    return (
+      <div className={ (this.props.className) ? ('banner-image ' + this.props.className) : 'banner-image' } ref='myRef'>
+        <div className='content-wrapper'>
+          { (this.props.text) ? <div className='names'>{ this.props.text }</div> : null }
+          {
+            (this.props.counter)
+            ? (<div className='counter'>
+              <div className='counter-item days'>{ this.state.days } d</div>
+              <div className='counter-item hours'>{ this.state.hours } h</div>
+              <div className='counter-item minutes'>{ this.state.minutes } m</div>
+              <div className='counter-item seconds'>{ this.state.seconds } s</div>
+            </div> )
+            : null
+          }
+        </div>
+      </div>
+    )
+  }
+};
+
+export default BannerImage;
